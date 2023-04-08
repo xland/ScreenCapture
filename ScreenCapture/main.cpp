241e295a1e6a8e5a22e390eb6d9858cdffa458c7
liulun <412588801@qq.com>
2023-04-08 22:03:36 +0800

    Update main.cpp

--- ScreenCapture/main.cpp
+++ ScreenCapture/main.cpp
@@ -1,18 +1,27 @@
-
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
+#include <Windows.h>
 
+
+static BOOL CALLBACK enumWindowCallback(HWND hwnd, LPARAM lparam) {
+    if(!IsWindowVisible(hwnd)) return TRUE;
+    if(IsIconic(hwnd)) return TRUE;
+    RECT rect;
+    GetWindowRect(hwnd,&rect);
+    if(rect.top <= 0 && rect.left <= 0 && rect.bottom <= 0 && rect.right <= 0) return TRUE;
+    qDebug() << hwnd;
+    return TRUE;
+}
 
 int main(int argc, char *argv[])
 {
     QGuiApplication app(argc, argv);
-
+EnumWindows(enumWindowCallback, NULL);
     QQmlApplicationEngine engine;
     const QUrl url(u"qrc:/ScreenCapture/Main.qml"_qs);
     QObject::connect(&engine, &QQmlApplicationEngine::objectCreationFailed,
         &app, []() { QCoreApplication::exit(-1); },
         Qt::QueuedConnection);
     engine.load(url);
-
     return app.exec();
 }
