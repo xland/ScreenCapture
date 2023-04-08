bf4eca406d67a66ae86a4a6b9fcaa5235a8c7e60
liulun <412588801@qq.com>
2023-04-08 19:33:36 +0800

    new project

---
+++ ScreenCapture/main.cpp
@@ -0,0 +1,18 @@
+
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+
+
+int main(int argc, char *argv[])
+{
+    QGuiApplication app(argc, argv);
+
+    QQmlApplicationEngine engine;
+    const QUrl url(u"qrc:/ScreenCapture/Main.qml"_qs);
+    QObject::connect(&engine, &QQmlApplicationEngine::objectCreationFailed,
+        &app, []() { QCoreApplication::exit(-1); },
+        Qt::QueuedConnection);
+    engine.load(url);
+
+    return app.exec();
+}
---
+++ ScreenCapture/main.qml
@@ -0,0 +1,9 @@
+import QtQuick
+import QtQuick.Window
+
+Window {
+    width: 640
+    height: 480
+    visible: true
+    title: qsTr("Hello World")
+}
