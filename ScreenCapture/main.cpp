64971cff5efc88ad73704ca5b8b6d6eec8c17134
liuxiaolun <xland@live.cn>
2023-04-15 16:41:13 +0800

    Update main.cpp

--- ScreenCapture/main.cpp
+++ ScreenCapture/main.cpp
@@ -1,23 +1,3 @@
-//#include <QGuiApplication>
-//#include <QQmlApplicationEngine>
-//
-//int main(int argc, char *argv[])
-//{
-//#if defined(Q_OS_WIN)
-//    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
-//#endif
-//
-//    QGuiApplication app(argc, argv);
-//
-//    QQmlApplicationEngine engine;
-//    engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
-//    if (engine.rootObjects().isEmpty())
-//        return -1;
-//
-//    return app.exec();
-//}
-
-
 #include <QApplication>
 #include <QQmlApplicationEngine>
 #include "ScreenImageProvider.h"
@@ -26,10 +6,8 @@
 
 int main(int argc, char* argv[])
 {
-    //    QGuiApplication::setHighDpiScaleFactorRoundingPolicy(Qt::HighDpiScaleFactorRoundingPolicy::Floor);
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
     QGuiApplication::setHighDpiScaleFactorRoundingPolicy(Qt::HighDpiScaleFactorRoundingPolicy::PassThrough);
-    //    qputenv("QT_SCALE_FACTOR", "1");
     QApplication  app(argc, argv);
     QQmlApplicationEngine engine;
     ScreenShoter::Init(&app);
