ec1f22f67adb34bc1abfdb2b3e78440a72561095
liulun <412588801@qq.com>
2023-08-02 19:29:41 +0800

    Ellipse drag resize

--- ScreenCapture/Shape/Ellipse.cpp
+++ ScreenCapture/Shape/Ellipse.cpp
@@ -34,6 +34,59 @@ namespace Shape {
         context->end();
         InvalidateRect(MainWin::Get()->hwnd, nullptr, false);
     }
+
+    void Ellipse::MouseInDragger(const double& x, const double& y)
+    {
+        for (size_t i = 0; i < 4; i++)
+        {
+            if (draggers[i].contains(x, y)) {
+                LPCTSTR cursor = IDC_SIZENESW;
+                switch (i)
+                {
+                case 0: {
+                    tempDraggerX = box.x1;
+                    tempDraggerY = box.y1;
+                    draggerIndex = i;
+                    cursor = IDC_SIZENWSE;
+                    break;
+                }
+                case 1:
+                {
+                    tempDraggerX = box.x0;
+                    tempDraggerY = box.y1;
+                    draggerIndex = i;
+                    cursor = IDC_SIZENESW;
+                    break;
+                }
+                case 2:
+                {
+                    tempDraggerX = box.x0;
+                    tempDraggerY = box.y0;
+                    draggerIndex = i;
+                    cursor = IDC_SIZENWSE;
+                    break;
+                }
+                case 3: {
+                    tempDraggerX = box.x1;
+                    tempDraggerY = box.y0;
+                    draggerIndex = i;
+                    cursor = IDC_SIZENESW;
+                    break;
+                }
+                }
+                ChangeCursor(cursor);
+                return;
+            }
+        }
+        if (box.contains(x, y)) {
+            draggerIndex = 4;
+            ChangeCursor(IDC_SIZEALL);
+            return;
+        }
+        draggerIndex = -1;
+        ChangeCursor(IDC_CROSS);
+    }
+
     void Ellipse::ShowDragger()
     {
         static int draggerSize = 6;
@@ -103,5 +156,42 @@ namespace Shape {
         win->state = win->preState;
         InvalidateRect(win->hwnd, nullptr, false);
         return true;
+    }
+
+
+    void Ellipse::DragDragger(const double& x, const double& y)
+    {
+        switch (draggerIndex)
+        {
+        case 0: {
+            Draw(x, y, tempDraggerX, tempDraggerY);
+            break;
+        }
+        case 1: {
+            Draw(tempDraggerX, y, x, tempDraggerY);
+            break;
+        }
+        case 2: {
+            Draw(tempDraggerX, tempDraggerY, x, y);
+            break;
+        }
+        case 3: {
+            Draw(x, tempDraggerY, tempDraggerX, y);
+            break;
+        }
+        case 4: {
+            auto win = MainWin::Get();
+            auto xSpan = x - win->MouseDownPos.x;
+            auto ySpan = y - win->MouseDownPos.y;
+            box.x0 += xSpan;
+            box.y0 += ySpan;
+            box.x1 += xSpan;
+            box.y1 += ySpan;
+            Draw(box.x0, box.y0, box.x1, box.y1);
+            win->MouseDownPos.x = x;
+            win->MouseDownPos.y = y;
+            break;
+        }
+        }
     }
 }
--- ScreenCapture/Shape/Ellipse.h
+++ ScreenCapture/Shape/Ellipse.h
@@ -4,6 +4,11 @@ namespace Shape {
 	class Ellipse : public Shape
 	{
 	public:
+		void Draw(const double& x1, const double& y1, const double& x2, const double& y2) override;
+		void ShowDragger() override;
+		void MouseInDragger(const double& x, const double& y) override;
+		bool EndDraw() override;
+		void DragDragger(const double& x, const double& y) override;
 		BLBox draggers[4];
 		bool isFill = false;
 		double rotateDeg = 0.0;
@@ -11,11 +16,9 @@ namespace Shape {
 		BLRgba32 color;
 		double strokeWidth;
 		BLBox box;
-		void Draw(const double& x1, const double& y1, const double& x2, const double& y2) override;
-		void ShowDragger() override;
-		bool EndDraw() override;
 	private:
 		int draggerIndex = -1;
+		double tempDraggerX, tempDraggerY;
 	};
 }
 
