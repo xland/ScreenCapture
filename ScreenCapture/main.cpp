0badaeb13ced93ad717de198a408cd174afd0ba2
unknown <liuxiaolun@hikvision.com>
2023-08-15 08:41:01 +0800

    Update main.cpp

--- ScreenCapture/main.cpp
+++ ScreenCapture/main.cpp
@@ -1,6 +1,5 @@
 ﻿#include "MainWin.h"
 #include <Windows.h>
-#include <iostream>
 
 
 int APIENTRY wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPTSTR lpCmdLine, _In_ int nCmdShow)
