a9c1ac221ae21d5e979096596f739a16ac905cf8
xland <412588801@qq.com>
2023-12-28 14:10:49 +0800

    Update ShapeNumber.h

--- src/Shape/ShapeNumber.h
+++ src/Shape/ShapeNumber.h
@@ -20,14 +20,12 @@ public:
 
 protected:
     bool isMouseOver(const int &x, const int &y);
-    void paintDragger(SkCanvas *canvas);
     void initParams();
     void makePath(const int &x1, const int &y1, const int &x2, const int &y2);
     SkPath path;
     bool stroke{true};
     int strokeWidth{4};
     SkColor color{SkColorSetARGB(255, 207, 19, 34)};
-    std::vector<SkRect> draggers;
     int endX, endY;
     int hoverX, hoverY;
     float r;
