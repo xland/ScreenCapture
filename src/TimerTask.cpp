0ef356edfb09b2345c7e049aff23f1bd7c02c024
xland <412588801@qq.com>
2024-01-15 16:59:44 +0800

    remove timer and shape dragger

--- src/TimerTask.cpp
+++
@@ -1,6 +0,0 @@
-#include "TimerTask.h"
-
-TimerTask::TimerTask(const int& id, std::chrono::steady_clock::time_point startTime, std::function<bool()> taskFunc) 
-	: Id{ id }, StartTime{ startTime }, TaskFunc{ taskFunc } {
-
-}
