ce4480680ec472db93429d33ab5d962e27e50a36
xland <412588801@qq.com>
2024-09-14 15:42:49 +0800

    Update ToolSub.cpp

--- ScreenCapture/ToolSub.cpp
+++ ScreenCapture/ToolSub.cpp
@@ -1,4 +1,5 @@
 #include "ToolSub.h"
+#include "CanvasWidget.h"
 
 namespace {
 	std::unique_ptr<ToolSub> toolSub;
@@ -15,6 +16,8 @@ ToolSub::~ToolSub()
 
 void ToolSub::Init()
 {
+	auto canvasWidget = CanvasWidget::Get();
+	toolSub = std::make_unique<ToolSub>();
 }
 
 void ToolSub::Get()
