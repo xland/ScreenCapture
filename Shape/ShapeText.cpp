8c1366009b2a1dd20d1bf58af55089ccb7997ef5
xland <412588801@qq.com>
2025-04-21 17:04:32 +0800

    Update ShapeText.cpp

--- Shape/ShapeText.cpp
+++ Shape/ShapeText.cpp
@@ -35,28 +35,10 @@ void ShapeText::paint(QPainter* painter)
     auto img = container->shapeTextInput->grab().toImage();
     auto win = (WinBase*)parent();
     img.setDevicePixelRatio(win->devicePixelRatio());
-    painter->drawImage(container->pos(), img);
+    painter->drawImage(container->pos()+container->shapeTextInput->pos(), img);
 }
 bool ShapeText::mouseMove(QMouseEvent* event)
 {
-    auto pos = event->position().toPoint();
-    auto win = (WinBase*)parent();
-    hoverDraggerIndex = -1;
-    if (container->isVisible()) {
-        if (!container->shapeTextInput->hasFocus()) {
-            container->hide();
-        }
-    }
-    else {
-        if (container->geometry().contains(pos)) {
-            //state = ShapeState::temp;
-            //win->winCanvas->clear();
-            //win->winCanvas->curShape = this;
-            //container->show();
-            //container->raise();
-            //event->accept();
-        }
-    }
     return false;
 }
 bool ShapeText::mousePress(QMouseEvent* event)
